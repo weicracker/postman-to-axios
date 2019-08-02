7d1d0f44d7f4311c6d940744c46a128e485059c1
weicracker <weicracker@163.com>
2019-08-02 14:26:07 +0800

    1

---
+++ cache/remote.js
@@ -0,0 +1,336 @@
+/* 流程中心 */
+import axios from "axios";
+import ActionBase from "../actbase";
+
+class TempAct extends ActionBase {
+    constructor() {
+        super();
+    }
+    static instance;
+    static get Instance() {
+        if (null == TempAct.instance) {
+            TempAct.instance = new TempAct();
+        }
+        return TempAct.instance;
+    }
+    async POSTAlreadyAssociation(page, ps,order='modifiedAsc') {
+        let ret = await axios.post(`${this.BaseURL}apps`, {
+            "page": page,
+            "rows": ps,
+            "tenantId": "tenantId001",
+            "order": order
+        });
+        if (ret.status == 200) {
+            return ret.data;
+        }
+    }
+    async POSTAlreadyAssociation(page, ps,order='modifiedAsc') {
+        let ret = await axios.post(`${this.BaseURL}apps`, {
+            "page": page,
+            "rows": ps,
+            "tenantId": "tenantId001",
+            "order": order
+        });
+        if (ret.status == 200) {
+            return ret.data;
+        }
+    }
+    async POSTAlreadyAssociation(page, ps,order='modifiedAsc') {
+        let ret = await axios.post(`${this.BaseURL}apps`, {
+            "page": page,
+            "rows": ps,
+            "tenantId": "tenantId001",
+            "order": order
+        });
+        if (ret.status == 200) {
+            return ret.data;
+        }
+    }
+    async DELETEAlreadyAssociation(page, ps,order='modifiedAsc') {
+        let ret = await axios.post(`${this.BaseURL}apps`, {
+            "page": page,
+            "rows": ps,
+            "tenantId": "tenantId001",
+            "order": order
+        });
+        if (ret.status == 200) {
+            return ret.data;
+        }
+    }
+    async POSTAlreadyAssociation(page, ps,order='modifiedAsc') {
+        let ret = await axios.post(`${this.BaseURL}apps`, {
+            "page": page,
+            "rows": ps,
+            "tenantId": "tenantId001",
+            "order": order
+        });
+        if (ret.status == 200) {
+            return ret.data;
+        }
+    }
+    async POSTAlreadyAssociation(page, ps,order='modifiedAsc') {
+        let ret = await axios.post(`${this.BaseURL}apps`, {
+            "page": page,
+            "rows": ps,
+            "tenantId": "tenantId001",
+            "order": order
+        });
+        if (ret.status == 200) {
+            return ret.data;
+        }
+    }
+    async DELETEAlreadyAssociation(page, ps,order='modifiedAsc') {
+        let ret = await axios.post(`${this.BaseURL}apps`, {
+            "page": page,
+            "rows": ps,
+            "tenantId": "tenantId001",
+            "order": order
+        });
+        if (ret.status == 200) {
+            return ret.data;
+        }
+    }
+    async POSTAlreadyAssociation(page, ps,order='modifiedAsc') {
+        let ret = await axios.post(`${this.BaseURL}apps`, {
+            "page": page,
+            "rows": ps,
+            "tenantId": "tenantId001",
+            "order": order
+        });
+        if (ret.status == 200) {
+            return ret.data;
+        }
+    }
+    async POSTAlreadyAssociation(page, ps,order='modifiedAsc') {
+        let ret = await axios.post(`${this.BaseURL}apps`, {
+            "page": page,
+            "rows": ps,
+            "tenantId": "tenantId001",
+            "order": order
+        });
+        if (ret.status == 200) {
+            return ret.data;
+        }
+    }
+    async POSTAlreadyAssociation(page, ps,order='modifiedAsc') {
+        let ret = await axios.post(`${this.BaseURL}apps`, {
+            "page": page,
+            "rows": ps,
+            "tenantId": "tenantId001",
+            "order": order
+        });
+        if (ret.status == 200) {
+            return ret.data;
+        }
+    }
+    async POSTAlreadyAssociation(page, ps,order='modifiedAsc') {
+        let ret = await axios.post(`${this.BaseURL}apps`, {
+            "page": page,
+            "rows": ps,
+            "tenantId": "tenantId001",
+            "order": order
+        });
+        if (ret.status == 200) {
+            return ret.data;
+        }
+    }
+    async GETAlreadyAssociation(page, ps,order='modifiedAsc') {
+        let ret = await axios.post(`${this.BaseURL}apps`, {
+            "page": page,
+            "rows": ps,
+            "tenantId": "tenantId001",
+            "order": order
+        });
+        if (ret.status == 200) {
+            return ret.data;
+        }
+    }
+    async POSTAlreadyAssociation(page, ps,order='modifiedAsc') {
+        let ret = await axios.post(`${this.BaseURL}apps`, {
+            "page": page,
+            "rows": ps,
+            "tenantId": "tenantId001",
+            "order": order
+        });
+        if (ret.status == 200) {
+            return ret.data;
+        }
+    }
+    async DELETEAlreadyAssociation(page, ps,order='modifiedAsc') {
+        let ret = await axios.post(`${this.BaseURL}apps`, {
+            "page": page,
+            "rows": ps,
+            "tenantId": "tenantId001",
+            "order": order
+        });
+        if (ret.status == 200) {
+            return ret.data;
+        }
+    }
+    async POSTAlreadyAssociation(page, ps,order='modifiedAsc') {
+        let ret = await axios.post(`${this.BaseURL}apps`, {
+            "page": page,
+            "rows": ps,
+            "tenantId": "tenantId001",
+            "order": order
+        });
+        if (ret.status == 200) {
+            return ret.data;
+        }
+    }
+    async POSTAlreadyAssociation(page, ps,order='modifiedAsc') {
+        let ret = await axios.post(`${this.BaseURL}apps`, {
+            "page": page,
+            "rows": ps,
+            "tenantId": "tenantId001",
+            "order": order
+        });
+        if (ret.status == 200) {
+            return ret.data;
+        }
+    }
+    async PUTAlreadyAssociation(page, ps,order='modifiedAsc') {
+        let ret = await axios.post(`${this.BaseURL}apps`, {
+            "page": page,
+            "rows": ps,
+            "tenantId": "tenantId001",
+            "order": order
+        });
+        if (ret.status == 200) {
+            return ret.data;
+        }
+    }
+    async PUTAlreadyAssociation(page, ps,order='modifiedAsc') {
+        let ret = await axios.post(`${this.BaseURL}apps`, {
+            "page": page,
+            "rows": ps,
+            "tenantId": "tenantId001",
+            "order": order
+        });
+        if (ret.status == 200) {
+            return ret.data;
+        }
+    }
+    async DELETEAlreadyAssociation(page, ps,order='modifiedAsc') {
+        let ret = await axios.post(`${this.BaseURL}apps`, {
+            "page": page,
+            "rows": ps,
+            "tenantId": "tenantId001",
+            "order": order
+        });
+        if (ret.status == 200) {
+            return ret.data;
+        }
+    }
+    async GETAlreadyAssociation(page, ps,order='modifiedAsc') {
+        let ret = await axios.post(`${this.BaseURL}apps`, {
+            "page": page,
+            "rows": ps,
+            "tenantId": "tenantId001",
+            "order": order
+        });
+        if (ret.status == 200) {
+            return ret.data;
+        }
+    }
+    async POSTAlreadyAssociation(page, ps,order='modifiedAsc') {
+        let ret = await axios.post(`${this.BaseURL}apps`, {
+            "page": page,
+            "rows": ps,
+            "tenantId": "tenantId001",
+            "order": order
+        });
+        if (ret.status == 200) {
+            return ret.data;
+        }
+    }
+    async POSTAlreadyAssociation(page, ps,order='modifiedAsc') {
+        let ret = await axios.post(`${this.BaseURL}apps`, {
+            "page": page,
+            "rows": ps,
+            "tenantId": "tenantId001",
+            "order": order
+        });
+        if (ret.status == 200) {
+            return ret.data;
+        }
+    }
+    async POSTAlreadyAssociation(page, ps,order='modifiedAsc') {
+        let ret = await axios.post(`${this.BaseURL}apps`, {
+            "page": page,
+            "rows": ps,
+            "tenantId": "tenantId001",
+            "order": order
+        });
+        if (ret.status == 200) {
+            return ret.data;
+        }
+    }
+    async POSTAlreadyAssociation(page, ps,order='modifiedAsc') {
+        let ret = await axios.post(`${this.BaseURL}apps`, {
+            "page": page,
+            "rows": ps,
+            "tenantId": "tenantId001",
+            "order": order
+        });
+        if (ret.status == 200) {
+            return ret.data;
+        }
+    }
+    async DELETEAlreadyAssociation(page, ps,order='modifiedAsc') {
+        let ret = await axios.post(`${this.BaseURL}apps`, {
+            "page": page,
+            "rows": ps,
+            "tenantId": "tenantId001",
+            "order": order
+        });
+        if (ret.status == 200) {
+            return ret.data;
+        }
+    }
+    async POSTAlreadyAssociation(page, ps,order='modifiedAsc') {
+        let ret = await axios.post(`${this.BaseURL}apps`, {
+            "page": page,
+            "rows": ps,
+            "tenantId": "tenantId001",
+            "order": order
+        });
+        if (ret.status == 200) {
+            return ret.data;
+        }
+    }
+    async POSTAlreadyAssociation(page, ps,order='modifiedAsc') {
+        let ret = await axios.post(`${this.BaseURL}apps`, {
+            "page": page,
+            "rows": ps,
+            "tenantId": "tenantId001",
+            "order": order
+        });
+        if (ret.status == 200) {
+            return ret.data;
+        }
+    }
+    async POSTAlreadyAssociation(page, ps,order='modifiedAsc') {
+        let ret = await axios.post(`${this.BaseURL}apps`, {
+            "page": page,
+            "rows": ps,
+            "tenantId": "tenantId001",
+            "order": order
+        });
+        if (ret.status == 200) {
+            return ret.data;
+        }
+    }
+    async DELETEAlreadyAssociation(page, ps,order='modifiedAsc') {
+        let ret = await axios.post(`${this.BaseURL}apps`, {
+            "page": page,
+            "rows": ps,
+            "tenantId": "tenantId001",
+            "order": order
+        });
+        if (ret.status == 200) {
+            return ret.data;
+        }
+    }
+}
+export default TempAct;
